564a47e378c033e22b83303d49334570acd96abf
daiane.goncalves <daiane.goncalves@ab-inbev.com>
2023-08-27 22:02:43 -0300

    Controller Methods started, database h2 configured and tested, dependencies included (JPA and validation)

--- api/src/main/java/msg/challenge/api/controller/MessageController.java
+++ api/src/main/java/msg/challenge/api/controller/MessageController.java
@@ -1,13 +1,44 @@
 package msg.challenge.api.controller;
 
+import jakarta.validation.Valid;
 import msg.challenge.api.message.MessageInfo;
+import msg.challenge.api.message.MessageRepository;
+import msg.challenge.api.message.MessageUpdate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
 
 @RestController
 @RequestMapping(value = "/messages")
 public class MessageController {
-    @PostMapping(value = "/input")
-    public void inputMessage(@RequestBody MessageInfo messageInfo){
-        System.out.println(messageInfo);
+
+    @Autowired
+    MessageRepository repository;
+    @PostMapping
+    public MessageInfo insert(@RequestBody MessageInfo message){
+        return repository.save(message);
     }
+
+    @GetMapping
+    public List<MessageInfo> findAll(){
+        return repository.findAll();
+    }
+
+    @GetMapping(value = "/{id}")
+    public MessageInfo findById(@PathVariable Long id) {
+        return repository.findById(id).get();
+    }
+
+    @PutMapping(value = "/{id}")
+    public void update(@PathVariable @Valid MessageUpdate message) {
+        var messageInfo = repository.getReferenceById(message.id);
+        messageInfo.updateInfo(message);
+    }
+
+//    @DeleteMapping("/{id}")
+//    public void delete(@PathVariable Long id) {
+//    }
+
 }
--- api/src/main/java/msg/challenge/api/message/MessageInfo.java
+++ api/src/main/java/msg/challenge/api/message/MessageInfo.java
@@ -1,27 +1,48 @@
 package msg.challenge.api.message;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+
+
+@Entity
+@Table(name = "tb_message")
 public class MessageInfo {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotBlank
     private String message;
 
-    public MessageInfo(String message) {
+    public MessageInfo(){
+        super();
+    }
+
+
+    public MessageInfo(Long id, String message) {
+        this();
+        this.id = id;
         this.message = message;
+    }
+
+    public Long getId() {
+        return id;
     }
 
     public String getMessage() {
         return message;
     }
 
-    public void setMessage(String message) {
-        this.message = message;
-    }
-
     @Override
     public String toString() {
-        return "Message{" +
+        return "MessageInfo{" +
                 "message='" + message + '\'' +
                 '}';
     }
 
-
+    public void updateInfo(MessageUpdate data) {
+        if (data.id != null){
+            this.message = data.message;
+        }
+    }
 }
